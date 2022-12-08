d6720322e2cdafb5a9d1ed7ad9a21dc3280d1c0b
Korhan Öztok <korhan.oeztok@bosch.com>
2022-12-08 13:24:53 +0100

    Create GraphQL schema and import it in Nest.js

--- nx-react-graphql-nestjs/apps/api/src/app/app.module.ts
+++ nx-react-graphql-nestjs/apps/api/src/app/app.module.ts
@@ -1,10 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      typePaths: ['./**/*.graphql'],
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
